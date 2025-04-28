559179c116d6b2baf6805836620326e280f0ca9a
Casper <casfro01@easv365.dk>
2025-04-28 21:07:39 +0200

    Abstrakt kontroller
    bruges primært til resizing og skifte vinduer

--- src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/GUI/IController.java
+++ src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/GUI/IController.java
@@ -1,4 +1,25 @@
 package dk.eksamensprojekt.belmaneksamensprojekt.GUI;
 
+import javafx.collections.ObservableList;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.text.Font;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
 public interface IController {
+    void initializeComponents(double width, double height);
+    void resizeItems(double width, double height);
+    void resizeItems(Map<Region, List<Double>> components, Map<ImageView, List<Double>> imageComponents, double width, double height);
+
+    void setControllerRoot(IController controller);
+    //void reload();
+    //AnchorPane getPane(Screens screen) throws IOException;
+
+    Font getFont(ObservableList<String> style, double newWidth, double newHeight);
+
+    //void setManager(ControllerManager manager);
 }
