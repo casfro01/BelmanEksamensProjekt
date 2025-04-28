9a72c3e88fce592e2752c0e64268f3b5f6e66c09
Casper <casfro01@easv365.dk>
2025-04-28 09:23:49 +0200

    Update Repository.java

--- src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/DAL/Repository.java
+++ src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/DAL/Repository.java
@@ -1,4 +1,12 @@
 package dk.eksamensprojekt.belmaneksamensprojekt.DAL;
 
+import java.util.List;
+
 public interface Repository<T, ID>{
+    List<T> getAll();
+    T getById(ID id);
+
+    T create(T entity);
+    void update(T entity);
+    void delete(T entity);
 }
