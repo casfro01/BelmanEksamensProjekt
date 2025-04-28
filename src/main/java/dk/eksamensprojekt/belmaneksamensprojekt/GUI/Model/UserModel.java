f4a42c94288fce6bfb39fb75a83d6cac4b25d23f
MiaSofie01 <miangaard@outlook.dk>
2025-04-28 13:04:55 +0200

    GUI lag users

---
+++ src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/GUI/Model/UserModel.java
@@ -0,0 +1,29 @@
+package dk.eksamensprojekt.belmaneksamensprojekt.GUI.Model;
+
+import dk.eksamensprojekt.belmaneksamensprojekt.BE.User;
+import dk.eksamensprojekt.belmaneksamensprojekt.BLL.UserManager;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.util.List;
+
+public class UserModel {
+    private UserManager userManager;
+
+    private ObservableList<User> users;
+
+    public UserModel() {
+        this.userManager = new UserManager();
+
+    }
+    private List<User> loadUsers() throws Exception {
+        return userManager.getAllUsers();
+    }
+    public ObservableList<User> getUsers() throws Exception {
+        if (users == null) {
+            users = FXCollections.observableArrayList();
+            users.addAll(loadUsers());
+        }
+        return users;
+    }
+}
