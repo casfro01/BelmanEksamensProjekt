f5248599f68ea0e9e6bdf5fd6c8c4e5c95e13420
Lucas <94841588+UsernameLuxi@users.noreply.github.com>
2025-04-28 12:06:34 +0200

    ihashing og login skabelon

---
+++ src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/BLL/util/PasswordHasher.java
@@ -0,0 +1,14 @@
+package dk.eksamensprojekt.belmaneksamensprojekt.BLL.util;
+
+public class PasswordHasher implements IHashing {
+
+    @Override
+    public String hashString(String input) throws Exception {
+        return "";
+    }
+
+    @Override
+    public boolean compare(String input, String hash) throws Exception {
+        return false;
+    }
+}
