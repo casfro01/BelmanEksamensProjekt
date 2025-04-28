7e134ba2555bd3d71f41e2dbd89cf3500a34389f
Lucas <94841588+UsernameLuxi@users.noreply.github.com>
2025-04-28 10:36:47 +0200

    OrderModel + OrderManager og OrderModel instans i ModelManager

---
+++ src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/BLL/OrderManager.java
@@ -0,0 +1,11 @@
+package dk.eksamensprojekt.belmaneksamensprojekt.BLL;
+
+import dk.eksamensprojekt.belmaneksamensprojekt.BE.Order;
+import javafx.collections.ObservableList;
+
+public class OrderManager {
+    public ObservableList<Order> getAllOrders() {
+        // call DAL to get all orders
+        return null;
+    }
+}
---
+++ src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/GUI/Model/OrderModel.java
@@ -0,0 +1,22 @@
+package dk.eksamensprojekt.belmaneksamensprojekt.GUI.Model;
+
+import dk.eksamensprojekt.belmaneksamensprojekt.BE.Order;
+import dk.eksamensprojekt.belmaneksamensprojekt.BLL.OrderManager;
+import javafx.collections.ObservableList;
+
+public class OrderModel {
+    private OrderManager orderManager;
+    private ObservableList<Order> orderList;
+
+    public OrderModel() {
+        this.orderManager = new OrderManager();
+    }
+
+    public ObservableList<Order> getOrderList() {
+        if (orderList.isEmpty()) {
+            orderList = orderManager.getAllOrders();
+        }
+
+        return orderList;
+    }
+}
