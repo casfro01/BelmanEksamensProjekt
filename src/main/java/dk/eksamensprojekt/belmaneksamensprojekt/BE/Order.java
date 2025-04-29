18d242097e5b284576f4bbf5436bffb0da1c9fe1
KodeGiraffen <morjen05@easv365.dk>
2025-04-29 11:57:17 +0200

    Update Order.java

--- src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/BE/Order.java
+++ src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/BE/Order.java
@@ -1,4 +1,29 @@
 package dk.eksamensprojekt.belmaneksamensprojekt.BE;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+    private int id; // primærenøgle
+    private Report report;
+    private String orderNumber;
+    private List<Image> images = new ArrayList<>();
+
+    public Order(int id, String orderNumber, Report report) {
+        this.id = id;
+        this.orderNumber = orderNumber; // setter til denne ? TODO : brug regex ?
+        this.report = report;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Report getReport() {
+        return report;
+    }
+
+    public List<Image> getImageList() {
+        return images;
+    }
 }
