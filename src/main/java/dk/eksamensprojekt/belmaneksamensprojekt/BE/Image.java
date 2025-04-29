596c7cd9ca57667d5548eeb586665d9e2016ec46
Lucas <94841588+UsernameLuxi@users.noreply.github.com>
2025-04-29 09:48:53 +0200

    image BE

--- src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/BE/Image.java
+++ src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/BE/Image.java
@@ -1,6 +1,29 @@
 package dk.eksamensprojekt.belmaneksamensprojekt.BE;
 
 public class Image {
-    // reference to image?
-    // string image text?
+    private int id;
+    private String path;
+    private boolean isApproved;
+
+    public Image(int id, String path, boolean isApproved) {
+        this.id = id;
+        this.path = path;
+        this.isApproved = isApproved;
+    }
+
+    private String getPath() {
+        return path;
+    }
+
+    private void setPath(String path) {
+        this.path = path;
+    }
+
+    private boolean isApproved() {
+        return isApproved;
+    }
+
+    private void setApproved(boolean approved) {
+        isApproved = approved;
+    }
 }
--- src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/BLL/OrderManager.java
+++ src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/BLL/OrderManager.java
@@ -11,7 +11,12 @@ public class OrderManager {
     public OrderManager() {
         this.ordersDAO = new OrdersDAO();
     }
+
     public List<Order> getAllOrders() throws Exception {
         return ordersDAO.getAll();
+    }
+
+    public void updateOrder(Order order) throws Exception {
+        ordersDAO.update(order);
     }
 }
