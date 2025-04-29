58504d7cc10d496b057eb8b3826c0b24a3faefce
KodeGiraffen <morjen05@easv365.dk>
2025-04-29 11:57:40 +0200

    Create PhotoDocumentController.java

---
+++ src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/GUI/Controllers/PhotoDocumentController.java
@@ -0,0 +1,38 @@
+package dk.eksamensprojekt.belmaneksamensprojekt.GUI.Controllers;
+
+import dk.eksamensprojekt.belmaneksamensprojekt.GUI.Controller;
+import dk.eksamensprojekt.belmaneksamensprojekt.GUI.Model.OrderModel;
+import dk.eksamensprojekt.belmaneksamensprojekt.GUI.ModelManager;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PhotoDocumentController extends Controller implements Initializable {
+    private ModelManager modelManager;
+    private OrderModel model;
+    @Override
+    public void initializeComponents(double width, double height) {
+        modelManager = ModelManager.getInstance();
+        model = modelManager.getOrderModel();
+        // KALD SUPER INITIALIZECOMPONENTS HER?? ANCHOR PANE???
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        initializeComponents(1920, 1080);
+    }
+
+    @FXML
+    private void takePictureClicked(ActionEvent event) throws Exception {
+        model.takePictureClicked();
+    }
+
+    @FXML
+    private void addPictureClicked(ActionEvent event) throws Exception {
+        model.addPictureClicked();
+    }
+}
