981eb9aecb0eae80f2e52cb96ffd65540031197b
Lucas <94841588+UsernameLuxi@users.noreply.github.com>
2025-04-29 09:58:16 +0200

    order tom fix

---
+++ src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/GUI/Controllers/PhotoDocumentController.java
@@ -0,0 +1,26 @@
+package dk.eksamensprojekt.belmaneksamensprojekt.GUI.Controllers;
+
+import dk.eksamensprojekt.belmaneksamensprojekt.GUI.Controller;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PhotoDocumentController extends Controller implements Initializable {
+
+    @Override
+    public void initializeComponents(double width, double height) {
+        // KALD SUPER INITIALIZECOMPONENTS HER?? ANCHOR PANE???
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        initializeComponents(1920, 1080);
+    }
+
+    public void takePicture() {
+
+    }
+
+    
+}
