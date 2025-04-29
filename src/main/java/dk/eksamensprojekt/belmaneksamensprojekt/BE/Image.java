ea8aa7d3a189bf5a104ea5fa3ad260eb86390074
KodeGiraffen <morjen05@easv365.dk>
2025-04-29 11:57:14 +0200

    Update Image.java

--- src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/BE/Image.java
+++ src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/BE/Image.java
@@ -1,6 +1,48 @@
 package dk.eksamensprojekt.belmaneksamensprojekt.BE;
 
 public class Image {
-    // reference to image?
-    // string image text?
+    private int id;
+    private String path;
+    private Approved isApproved;
+    private User user;
+    private int orderID;
+
+    public Image(int id, String path, Approved isApproved) {
+        this.id = id;
+        this.path = path;
+        this.isApproved = isApproved;
+    }
+
+    public Image(int id, String path, Approved isApproved, User user, int orderID) {
+        this.id = id;
+        this.path = path;
+        this.isApproved = isApproved;
+        this.user = user;
+        this.orderID = orderID;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public Approved isApproved() {
+        return isApproved;
+    }
+
+    public void setApproved(Approved approved) {
+        isApproved = approved;
+    }
+    public int getId() {
+        return id;
+    }
+    public User getUser(){
+        return user;
+    }
+    public int getOrderID() {
+        return orderID;
+    }
 }
