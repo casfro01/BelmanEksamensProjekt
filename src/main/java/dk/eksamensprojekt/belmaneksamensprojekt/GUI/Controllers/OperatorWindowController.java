2ff6d92731f9551933e063327f2dc4d47b7c8ed5
Casper <casfro01@easv365.dk>
2025-05-04 19:57:37 +0200

    forbindelse af operatorvinduer

--- src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/GUI/Controllers/OperatorWindowController.java
+++ src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/GUI/Controllers/OperatorWindowController.java
@@ -1,6 +1,22 @@
 package dk.eksamensprojekt.belmaneksamensprojekt.GUI.Controllers;
 
+import dk.eksamensprojekt.belmaneksamensprojekt.GUI.Commands.SwitchWindowCommand;
 import dk.eksamensprojekt.belmaneksamensprojekt.GUI.Controller;
+import dk.eksamensprojekt.belmaneksamensprojekt.GUI.util.Windows;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 public class OperatorWindowController extends Controller {
+    @FXML
+    private TextField txtSearchOrdernumb;
+
+    @FXML
+    private void onEnterPressed(KeyEvent keyEvent) {
+        if (keyEvent.getCode() == KeyCode.ENTER){
+            // do things
+            getInvoker().executeCommand(new SwitchWindowCommand(Windows.PhotoDocWindow));
+        }
+    }
 }
