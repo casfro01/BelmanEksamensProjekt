47b37688464aaf811c94ab7042a74a57eddf122f
Casper <casfro01@easv365.dk>
2025-05-05 21:25:14 +0200

    billeder

--- src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/GUI/Controllers/TopBarController.java
+++ src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/GUI/Controllers/TopBarController.java
@@ -1,6 +1,18 @@
 package dk.eksamensprojekt.belmaneksamensprojekt.GUI.Controllers;
 
 import dk.eksamensprojekt.belmaneksamensprojekt.GUI.Controller;
+import javafx.fxml.Initializable;
 
-public class TopBarController extends Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TopBarController extends Controller implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // indsæt hvem der er logget ind
+    }
+
+    // logout metode
+
+    // back metode
 }
