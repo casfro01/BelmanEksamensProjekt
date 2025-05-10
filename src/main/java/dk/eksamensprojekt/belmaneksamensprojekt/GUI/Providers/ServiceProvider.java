45420d6855b9374eb3a5307fc60453a1987ef0c3
Casper <casfro01@easv365.dk>
2025-05-10 18:52:01 +0200

    kommentar rettelse

--- src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/GUI/Providers/ServiceProvider.java
+++ src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/GUI/Providers/ServiceProvider.java
@@ -5,7 +5,7 @@ import dk.eksamensprojekt.belmaneksamensprojekt.GUI.Commands.SwitchWindowCommand
 import dk.eksamensprojekt.belmaneksamensprojekt.GUI.WindowService;
 
 public class ServiceProvider {
-    // Hvis dette skal opskaleres, så ændre det til et map, så man er fri for at lave så mange ifs
+    // Hvis dette skal opskalere, så ændre det til et map, så man er fri for at lave så mange ifs
     private static WindowService windowService;
     public static WindowService getServiceForCommand(Command cmd) {
         if (cmd instanceof SwitchWindowCommand){
