96d247d17f5a809411e2133e4ef2ea3e4fb6f6e1
Lucas <94841588+UsernameLuxi@users.noreply.github.com>
2025-05-21 13:37:07 +0200

    Extra image scaling + picture limit + submit requirement

--- src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/Constants/Constants.java
+++ src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/Constants/Constants.java
@@ -7,9 +7,13 @@ import java.io.File;
 public class Constants {
     public static final String IMAGES_PATH = System.getProperty("user.dir") + File.separator + "Images" + File.separator;
     public static double DISPLAY_TIME = 1.5;
+    public static final int SMALL_ORDER_MAX = 10;
+    public static final int SMALL_ORDER_MIN = 5;
+    public static final int LARGE_ORDER_MAX = 100;
+    public static final int LARGE_ORDER_MIN = 5;
 
 
-    public static void DisplayError(String title, Exception ex) {
-        ShowAlerts.splashMessage(title, ex.getMessage(), DISPLAY_TIME);
+    public static void DisplayError(String title, String message) {
+        ShowAlerts.splashMessage(title, message, DISPLAY_TIME);
     }
 }
