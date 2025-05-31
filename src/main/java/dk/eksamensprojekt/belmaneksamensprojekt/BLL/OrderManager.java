7c903689f4075027391995066d86e61773a7928b
Casper <casfro01@easv365.dk>
2025-05-31 21:49:47 +0200

    BLL

--- src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/BLL/OrderManager.java
+++ src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/BLL/OrderManager.java
@@ -1,5 +1,6 @@
 package dk.eksamensprojekt.belmaneksamensprojekt.BLL;
 
+// Projekt imports
 import dk.eksamensprojekt.belmaneksamensprojekt.BE.*;
 import dk.eksamensprojekt.belmaneksamensprojekt.BE.Enums.Approved;
 import dk.eksamensprojekt.belmaneksamensprojekt.BE.Enums.ImagePosition;
@@ -7,12 +8,17 @@ import dk.eksamensprojekt.belmaneksamensprojekt.BE.Image;
 import dk.eksamensprojekt.belmaneksamensprojekt.DAL.OrderDaoFacade;
 import dk.eksamensprojekt.belmaneksamensprojekt.DAL.Repository;
 
+// Java imports
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.List;
 
+/**
+ * Manager til at håndtere ordre
+ */
 public class OrderManager {
+    // Statisk konstant -> til billede mappen. (Desuden ligger en magen til i Image-klassen)
     private static final String IMAGES_PATH = System.getProperty("user.dir") + File.separator + "Images" + File.separator;
 
     private final Repository<Order, String> ordersDAO;
@@ -29,33 +35,49 @@ public class OrderManager {
         ordersDAO.update(order);
     }
 
+    /**
+     * Åbner brugerens kamera, så brugeren kan tilføje et billede til en bestemt ordre.
+     * @param order Den ordre som brugeren skal tage et billede til.
+     * @param user Den bruger som tager billede (bruges til at placere ansvar).
+     * @param imagePosition Den vinkel som billedet repræsenterer på produktet.
+     * @return Det billede som blev taget.
+     * @throws Exception Hvis der sker en fejl, enten fordi den ikke kan åbne kameraet, finde din filmappe eller en anden årsag, så kommer der en Exception.
+     */
     public Image openCamera(Order order, User user, ImagePosition imagePosition) throws Exception {
         try {
+            // åben kameraet
             Runtime.getRuntime().exec("cmd.exe /C start microsoft.windows.camera:");
 
+            // find brugerens billede mappe, hvor billederne komme hen, når et billede tages med kameraet
             String userHome = System.getProperty("user.home");
             Path cameraRoll = Paths.get(userHome, "Pictures", "Camera Roll");
+
+            // prøver at finde hvilken type mappe der skal bruges -> hvis onedrive er tilsluttet.
             if (!Files.exists(cameraRoll)) {
                 cameraRoll = Paths.get(userHome, "OneDrive", "Pictures", "Filmrulle");
             }
 
             if (!Files.exists(cameraRoll)) {
-                throw new RuntimeException("Camera Roll not found");
+                throw new Exception("Camera Roll not found");
             }
 
+            // tilføjer en listener for at lytte efter et kommende billede.
             WatchService watchService = FileSystems.getDefault().newWatchService();
             cameraRoll.register(watchService, StandardWatchEventKinds.ENTRY_CREATE);
 
+            // venter på at der sker noget
             WatchKey key;
             while ((key = watchService.take()) != null) {
                 for (WatchEvent<?> event : key.pollEvents()) {
+                    // hvis den finder et billede
                     if (event.kind() == StandardWatchEventKinds.ENTRY_CREATE &&
                             event.context().toString().endsWith(".jpg")) {
 
+                        // klargør billede
                         Path imagePath = cameraRoll.resolve((Path) event.context());
                         Path fileName = imagePath.getFileName();
                         Path newLocation = Paths.get(IMAGES_PATH + fileName);
-
+                        // Kopierer billedet over på fælles mappen/floder
                         boolean fileReady = waitForFileReady(imagePath, 500, 10); // 500ms interval, 10 retries
                         if (fileReady) {
                             Files.copy(imagePath, newLocation);
@@ -63,6 +85,7 @@ public class OrderManager {
                             throw new Exception("Error accessing picture taken.");
                         }
 
+                        // Generere nyt billede objekt
                         Image image = new Image(
                                 -1,
                                 fileName.toString(),
@@ -72,6 +95,7 @@ public class OrderManager {
                                 imagePosition
                         );
 
+                        // Luk kamera appen og sende billede til gui-laget
                         Runtime.getRuntime().exec("taskkill /IM WindowsCamera.exe /F");
                         return image;
                     }
@@ -79,12 +103,19 @@ public class OrderManager {
                 key.reset();
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            throw new Exception("Error with camera", e.getCause());
         }
 
         return null;
     }
 
+    /**
+     * Venter på filen på den givne sti til at blive stabil i størrelse, for at sikre at billedet/filen ikke skaber problemer.
+     * @param path stien til filen
+     * @param waitMillis mængden af tid som der skal ventes mellem hvert interval i millisekunder.
+     * @param retries hvor mange forsøg der bliver brugt på at tjekke filens status.
+     * @return hvis filen er stabil -> true, hvis filen er ustabil -> false.
+     */
     private static boolean waitForFileReady(Path path, long waitMillis, int retries) {
         long previousSize = -1;
         try {
@@ -104,6 +135,9 @@ public class OrderManager {
         return false;
     }
 
+    /**
+     * Gem ordre status.
+     */
     public void submitOrder(Order currentOrder) throws Exception {
         currentOrder.setDocumented(true);
         ordersDAO.update(currentOrder);
--- src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/BLL/UserManager.java
+++ src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/BLL/UserManager.java
@@ -1,5 +1,6 @@
 package dk.eksamensprojekt.belmaneksamensprojekt.BLL;
 
+// Projekt imports
 import dk.eksamensprojekt.belmaneksamensprojekt.BE.LoginUser;
 import dk.eksamensprojekt.belmaneksamensprojekt.BE.User;
 import dk.eksamensprojekt.belmaneksamensprojekt.BLL.util.EmailValidator;
@@ -9,10 +10,12 @@ import dk.eksamensprojekt.belmaneksamensprojekt.DAL.Repository;
 import dk.eksamensprojekt.belmaneksamensprojekt.DAL.UserDAO;
 import dk.eksamensprojekt.belmaneksamensprojekt.DAL.UserData;
 
-import javax.management.relation.Role;
+// Java imports
 import java.util.List;
-import java.util.regex.Pattern;
 
+/**
+ * Manager til at håndtere brugere(Users)
+ */
 public class UserManager {
     private final Repository<User, Integer> userDAO;
     private final IHashing passwordHashing;
@@ -26,20 +29,37 @@ public class UserManager {
         return userDAO.getAll();
     }
 
+    /**
+     * Login. Send email og password(not hashed) ind og hvis login lykkes, bliver den fulde bruger returneret.
+     * @param email Brugerens email.
+     * @param password Brugerens password
+     * @return Hvis informationerne er korrekt så bliver den fulde bruger sendt tilbage, ellers bliver null returneret
+     *
+     * (TODO: Brude flyttes til LoginManager)
+     */
     public User login(String email, String password) throws Exception {
         // validér emailen
         if (!EmailValidator.validate(email))
             throw new IllegalArgumentException("Invalid email address");
 
+        // Tjekker om UserDAO kan håndtere login
         if (userDAO instanceof UserData uData){
             LoginUser loginUser = uData.getLoginUser(email);
+            // Validér login
             if (loginUser != null && passwordHashing.compare(password, loginUser.getPassword()))
-                return userDAO.getById(loginUser.getUserID());
+                return userDAO.getById(loginUser.getUserID()); // henter brugeren med id'et
             return null;
         }
         else
             throw new Exception("Login Failed: This Login type is not supported");
     }
+
+    /**
+     * Lav en bruger
+     * @param user Bruger objektet med diverse informationer på (uden id)
+     * @param loginUser Brugerens login oplysninger - hvis de findes
+     * @return Den nyligt lavet bruger, med id fra databasen
+     */
     public User create(User user, LoginUser loginUser) throws Exception {
         if (user.getName() == null || user.getName().isEmpty())
             throw new Exception("Name is required");
@@ -50,7 +70,7 @@ public class UserManager {
         if (loginUser.getPassword() == null || loginUser.getPassword().isEmpty())
             throw new Exception("Password not set");
 
-        User newUser = userDAO.create(user);
+        User newUser = userDAO.create(user); // gemmer bruger i database
         newUser.setImagePath(user.getImagePath());
 
         // gem login informationer
