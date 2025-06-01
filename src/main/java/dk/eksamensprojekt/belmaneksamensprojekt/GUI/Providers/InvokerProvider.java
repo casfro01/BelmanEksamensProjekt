2c1d31b0aa881bef5f84f9bf9fa9204beb904fa8
Casper <casfro01@easv365.dk>
2025-06-01 16:35:56 +0200

    GUI; Providers Kommentarer

--- src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/GUI/Providers/InvokerProvider.java
+++ src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/GUI/Providers/InvokerProvider.java
@@ -2,9 +2,15 @@ package dk.eksamensprojekt.belmaneksamensprojekt.GUI.Providers;
 
 import dk.eksamensprojekt.belmaneksamensprojekt.GUI.WindowInvoker;
 
+/**
+ * Denne klasse har det formål at stille en invoker til rådighed -> deraf er det en provider.
+ */
 public class InvokerProvider {
     private static WindowInvoker invoker = null;
 
+    /**
+     * Hent en invoker som kan udføre kommandoer.
+     */
     public static WindowInvoker getInvoker() {
         if (invoker == null) {
             invoker = new WindowInvoker();
--- src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/GUI/Providers/ServiceProvider.java
+++ src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/GUI/Providers/ServiceProvider.java
@@ -4,9 +4,19 @@ import dk.eksamensprojekt.belmaneksamensprojekt.GUI.Commands.Command;
 import dk.eksamensprojekt.belmaneksamensprojekt.GUI.Commands.SwitchWindowCommand;
 import dk.eksamensprojekt.belmaneksamensprojekt.GUI.Services.WindowService;
 
+/**
+ * Denne klasse har til formål at stille services til rådighed (kan erstattes af DI)
+ */
 public class ServiceProvider {
-    // Hvis dette skal opskalere, så ændre det til et map, så man er fri for at lave så mange ifs
+    // Hvis dette skal opskalere, så ændre det til et map, så man er fri for at lave så mange IFs
     private static WindowService windowService;
+
+    /**
+     * Hent den service som kommandoerne benytter til at udføre deres opgave.
+     * (Denne metode indkapsler data -> så hvis man ikke er en kommando, så kan man ikke få fat i servicen).
+     * @param cmd Den {@link Command} som kræver servicen.
+     * @return {@link WindowService}
+     */
     public static WindowService getServiceForCommand(Command cmd) {
         if (cmd instanceof SwitchWindowCommand){
             return windowService;
@@ -15,6 +25,9 @@ public class ServiceProvider {
         return null;
     }
 
+    /**
+     * Sæt den aktive {@link WindowService}.
+     */
     public static void setWindowService(WindowService windowService) {
         ServiceProvider.windowService = windowService;
     }
