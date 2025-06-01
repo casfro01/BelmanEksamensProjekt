1136dac508c0d92c72626f32934ae215fc16b848
Casper <casfro01@easv365.dk>
2025-06-01 17:03:50 +0200

    GUI; Services kommentarer

--- src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/GUI/Services/MainWindowService.java
+++ src/main/java/dk/eksamensprojekt/belmaneksamensprojekt/GUI/Services/MainWindowService.java
@@ -1,29 +1,42 @@
 package dk.eksamensprojekt.belmaneksamensprojekt.GUI.Services;
 
+// Projekt imports
 import dk.eksamensprojekt.belmaneksamensprojekt.GUI.Controllers.MainWindowController;
 import dk.eksamensprojekt.belmaneksamensprojekt.GUI.util.MainWindowViews;
 import dk.eksamensprojekt.belmaneksamensprojekt.GUI.util.ShowAlerts;
 import dk.eksamensprojekt.belmaneksamensprojekt.Main;
+
+// JavaFX
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Alert;
 import javafx.scene.layout.AnchorPane;
 
+// Java
 import java.io.IOException;
 
+/**
+ * Denne service fokuserer på at skifte viewet inde i main-vinduet (altså små vinduer).
+ */
 public class MainWindowService {
+    // Denne service kræver main-vinduet -> dette gør a kommandoerne ikke skal kende til main-kontrolleren
     private MainWindowController mwc;
 
     public MainWindowService(MainWindowController mwc) {
         this.mwc = mwc;
     }
 
+    /**
+     * Sætter et view på main-vinduet.
+     * @param mainWView Dét {@link MainWindowViews} som skal vises.
+     */
     public void setView(MainWindowViews mainWView) {
         try {
+            // Load vinduet
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(mainWView.getPath()));
             AnchorPane ap = fxmlLoader.load();
+            // sæt vinduet ind på main-vinduet.
             mwc.setView(ap);
         } catch (IOException e) {
-            ShowAlerts.displayMessage("Window Error", "Could not find view!", Alert.AlertType.ERROR);
+            ShowAlerts.displayError("Window Error", "Could not find view!");
         }
     }
 }
